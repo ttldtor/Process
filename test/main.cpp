a0841cfb66a583c3a95fbe73eb104481c37e03df
Anatoly Kalin <anatoly.kalin@devexperts.com>
2023-09-11 13:49:20 +0300

    Add getting CPU usage information on Linux.

--- test/main.cpp
+++ test/main.cpp
@@ -1,17 +1,19 @@
 #include <process/process.hpp>
 
+#include <chrono>
 #include <iostream>
 #include <thread>
-#include <chrono>
 #include <vector>
 
 using namespace std::literals;
 using namespace ttldtor::process;
 
 int main() {
-    auto now = [] { return duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now().time_since_epoch()).count(); };
+    auto now = [] {
+        return duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
+    };
     std::cout << "Physical memory usage: " << Process::getPhysicalMemorySize() / 1024 << "KB" << std::endl;
-    std::cout << "Total CPU time: " << Process::getTotalProcessorTime() << std::endl;
+    std::cout << "Total CPU time: " << Process::getTotalProcessorTime().count() << "ms" << std::endl;
 
     auto t1 = now();
 
@@ -20,9 +22,9 @@ int main() {
         (void)(i++);
     }
 
-    std::cout << "Total CPU time: " <<  Process::getTotalProcessorTime() << std::endl;
+    std::cout << "Total CPU time: " << Process::getTotalProcessorTime().count() << "ms" << std::endl;
     std::cout << "Physical memory usage: " << Process::getPhysicalMemorySize() / 1024 << "KB" << std::endl;
-    std::vector<std::byte> vb(10000000);
+    std::vector<std::byte> vb(10 * 1024 * 1024);
     std::cout << "Physical memory usage: " << Process::getPhysicalMemorySize() / 1024 << "KB" << std::endl;
     vb.resize(1);
     vb.shrink_to_fit();
